d8c53af7d2b211199a90e980e2cb54db4e7a6796
HiiMark1 <65589740+HiiMark1@users.noreply.github.com>
2021-12-28 11:07:09 +0300

    fix index

--- indexes, group by/V24__indexes.sql
+++ indexes, group by/V24__indexes.sql
@@ -1,5 +1,5 @@
 --Для быстрого сравнения равенства регионов по id 
 CREATE INDEX region_region_id_hash_idx ON region USING hash(region_id);
 
---Для отсортированного вывода заказов по id
-CREATE INDEX orders_id_idx ON orders USING btree(orders_id);
+--Для отсортированного вывода заказов по кол-ву требуемого продукта
+CREATE INDEX orders_number_idx ON orders USING btree(number);
